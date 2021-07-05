887862ec43dda3868a0c58e7f5a8b11e03d764c2
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-05 13:13:07 -0600

    chore: Add DeviceMacSubstringByType TS type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import { Context as ApolloContext } from 'apollo-server-core';
+import { Context as ApolloContext } from "apollo-server-core";
 
 export interface Context extends ApolloContext {
   prisma: PrismaClient;
+}
+
+export enum DeviceMacSubstringByType {
+  NANOLEAF = "02:55:da",
+  LIFX = "d0:73:d5",
+  HUE = "ECB5",
 }
