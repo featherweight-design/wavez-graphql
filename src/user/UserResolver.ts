f776d93f1aca71fbb6d7a41b28e6e5bb4f3b06ba
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-05 11:15:00 -0600

    build: Inital User Query/Mutation

---
+++ src/user/UserResolver.ts
@@ -0,0 +1,32 @@
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+
+import { Context } from "types";
+import User from "./User";
+import { CreateUserInput } from "./UserInputs";
+
+@Resolver(User)
+class UserResolver {
+  @Query(() => User, { nullable: true })
+  async getUserById(
+    @Arg("id") id: string,
+    @Ctx() { prisma }: Context
+  ): Promise<User | null> {
+    const user = await prisma.user.findUnique({ where: { id } });
+
+    return user;
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg("input") input: CreateUserInput,
+    @Ctx() { prisma }: Context
+  ): Promise<User> {
+    const user = prisma.user.create({
+      data: input,
+    });
+
+    return user;
+  }
+}
+
+export default UserResolver;
