78277e418ee7526e870d4dfd52bd158ad4022de7
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-06 20:20:03 -0600

    feat: Create authenticateWithNanoleafDevice util

---
+++ src/nanoleaf/utils/authenticateWithNanoleafDevice.ts
@@ -0,0 +1,28 @@
+import { UserInputError } from "apollo-server";
+import fetch from "node-fetch";
+
+import { NanoleafAuthenticationResponse } from "types";
+import { constants, errors } from "../definitions";
+
+const { endpoints } = constants;
+
+const authenticateWithNanoleafDevice = async (
+  ipAddress: string
+): Promise<string> => {
+  const response = await fetch(endpoints.authenticate(ipAddress), {
+    method: "POST",
+  });
+
+  if (!response.ok && (response.status === 401 || response.status === 403)) {
+    const error = errors.auth(response.status);
+
+    throw new UserInputError(error.message, error);
+  }
+
+  const { auth_token: authToken }: NanoleafAuthenticationResponse =
+    await response.json();
+
+  return authToken;
+};
+
+export default authenticateWithNanoleafDevice;
