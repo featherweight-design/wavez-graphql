d5ad2eb07f49638b23a9fabffb4b81b4a7a1a0fa
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-06 20:19:13 -0600

    build: Initial Nanoleaf errors

---
+++ src/nanoleaf/definitions/errors.ts
@@ -0,0 +1,15 @@
+const errors = {
+  auth: (status: number) => ({
+    status,
+    message: "Unable to authenticate with Nanoleaf device",
+    friendlyMessage:
+      "Unable to authenticate with Nanoleaf device. Ensure that your device is ready to authenticate by holding down the power button until the lights start blinking (5-7 seconds).",
+  }),
+  deviceConflict: (ipAddress: string) => ({
+    status: 409,
+    message: `Device conflict by IP Address ${ipAddress}`,
+    friendlyMessage: `A device using the IP Address ${ipAddress} already exists.`,
+  }),
+};
+
+export default errors;
