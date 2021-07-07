a5fd8d35c136f81de8b6107df7e125c0fc67003f
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-06 20:21:03 -0600

    feat: Create authenticateWithNanoleafDevice util

---
+++ src/nanoleaf/utils/doesDeviceExistsByIpAddress.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { UserInputError } from "apollo-server";
+import { errors } from "nanoleaf/definitions";
+
+const doesDeviceExistsByIpAddress = async (
+  prisma: PrismaClient,
+  ipAddress: string
+) => {
+  const doesDeviceExist = await prisma.device.findUnique({
+    where: {
+      ip: ipAddress,
+    },
+  });
+
+  //* IP addresses much be unique, so throw error accordingly
+  if (doesDeviceExist) {
+    const error = errors.deviceConflict(ipAddress);
+
+    throw new UserInputError(error.message, error);
+  }
+};
+
+export default doesDeviceExistsByIpAddress;
