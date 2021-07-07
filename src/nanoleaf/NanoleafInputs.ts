4e25163f6fa9fd5d09c1be98c99058781bf89c91
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-07 06:51:48 -0600

    chore: Build UpdateStateInput

--- src/nanoleaf/NanoleafInputs.ts
+++ src/nanoleaf/NanoleafInputs.ts
@@ -1,5 +1,8 @@
 import { Field, InputType } from "type-graphql";
 
+import { NanoleafStateValueProps } from "types";
+import { NanoleafStateResolver, NanoleafStateValue } from "nanoleaf/state";
+
 @InputType()
 class AuthenticateNewUserInput {
   @Field()
@@ -12,4 +15,25 @@ class AuthenticateNewUserInput {
   mac: string;
 }
 
-export { AuthenticateNewUserInput };
+@InputType()
+class UpdateStateInput {
+  @Field()
+  on?: NanoleafStateValueProps;
+
+  @Field()
+  brightness?: NanoleafStateValueProps;
+
+  @Field()
+  hue?: NanoleafStateValueProps;
+
+  @Field()
+  sat?: NanoleafStateValueProps;
+
+  @Field()
+  ct?: NanoleafStateValueProps;
+
+  @Field()
+  colorMode?: string;
+}
+
+export { AuthenticateNewUserInput, UpdateStateInput };
