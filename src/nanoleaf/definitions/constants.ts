de43095be3e4150e074f5ba742f3d6748f69c23b
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-07 05:22:20 -0600

    chore: Update endpoints

--- src/nanoleaf/definitions/constants.ts
+++ src/nanoleaf/definitions/constants.ts
@@ -1,8 +1,12 @@
 const endpoints = {
   baseApi: (ipAddress: string) => `http://${ipAddress}:16021/api/v1`,
   authenticate: (ipAddress: string) => `${endpoints.baseApi(ipAddress)}/new`,
-  getAllProperties: (ipAddress: string, authToken: string) =>
-    `${endpoints.baseApi(ipAddress)}/${authToken}`,
+  get: {
+    effectsList: (ipAddress: string, authToken: string) =>
+      `${endpoints.baseApi(ipAddress)}/${authToken}/effects/effectsList`,
+    properties: (ipAddress: string, authToken: string) =>
+      `${endpoints.baseApi(ipAddress)}/${authToken}`,
+  },
   update: {
     effect: (ipAddress: string, authToken: string) =>
       `${endpoints.baseApi(ipAddress)}/${authToken}/effects`,
