57a00e54693ce2d1e9d8b539f7eb60207f5f1aa3
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-07 21:30:53 -0600

    fix: Build checkForOnState helper in updateCurrentState utility due to non-union input types

--- src/nanoleaf/utils/updateCurrentState.ts
+++ src/nanoleaf/utils/updateCurrentState.ts
@@ -3,6 +3,24 @@ import { constants } from "nanoleaf/definitions";
 
 const { endpoints } = constants;
 
+/**
+ * Type GraphQL does not allow union types for inputs, so
+ * we need to pass a string of "true" to update the "on"
+ * state. This converts it to a boolen for the Nanoleaf API.
+ * See: https://github.com/MichalLytek/type-graphql/issues/384
+ */
+const checkForOnState = (body: NanoleafStateProps) => {
+  if (body.on) {
+    return {
+      on: {
+        value: body.on.value === "true",
+      },
+    };
+  }
+
+  return body;
+};
+
 const updateCurrentState = async (
   ipAddress: string,
   authToken: string,
@@ -11,7 +29,7 @@ const updateCurrentState = async (
   try {
     await fetch(endpoints.update.state(ipAddress, authToken), {
       method: "PUT",
-      body: JSON.stringify(body),
+      body: JSON.stringify(checkForOnState(body)),
     });
   } catch (error) {
     console.error(error);
