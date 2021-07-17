9faf7f9f2ff745dc3c80349c92172a156279b49d
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-17 16:30:08 -0600

    feat: Write getAllDevicesByUserId query and user FieldResolver

--- src/device/DeviceResolver.ts
+++ src/device/DeviceResolver.ts
@@ -1,14 +1,28 @@
-import { Arg, Query, Resolver } from 'type-graphql';
+import Prisma from '.prisma/client';
+import { Arg, Ctx, FieldResolver, Query, Resolver, Root } from 'type-graphql';
 import find from 'local-devices';
 
-import { DeviceMacSubstringByType, DeviceTypeEnum } from 'types';
+import { Context, DeviceMacSubstringByType, DeviceTypeEnum } from 'types';
 import { Device, WifiDevice } from './Device';
+import { User } from 'user';
 
 const findeDeviceByType = (device: WifiDevice, macSubstring: string) =>
   device.mac.toLocaleLowerCase().includes(macSubstring.toLocaleLowerCase());
 
 @Resolver(Device)
 class DeviceResolver {
+  @FieldResolver(() => User)
+  async user(
+    @Root() device: Device,
+    @Ctx() { prisma }: Context
+  ): Promise<User | null> {
+    const user = await prisma.device
+      .findUnique({ where: { id: device.id } })
+      .user();
+
+    return user;
+  }
+
   @Query(() => [WifiDevice])
   async discoverWifiDevices(): Promise<WifiDevice[]> {
     const devices = await find();
@@ -29,6 +43,21 @@ class DeviceResolver {
     });
 
     return filteredDevices;
+  }
+
+  // eslint-disable-next-line type-graphql/wrong-decorator-signature
+  @Query(() => [Device])
+  async getAllDevicesByUserId(
+    @Arg('userId') userId: string,
+    @Ctx() { prisma }: Context
+  ): Promise<Prisma.Device[]> {
+    const devices = await prisma.device.findMany({
+      where: {
+        userId,
+      },
+    });
+
+    return devices;
   }
 }
 
