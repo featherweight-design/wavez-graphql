5647ab907f03f3bea32f90c093f4d61d43b3ae8c
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-24 13:41:37 -0600

    feat: Build updateEffectName utility

---
+++ src/nanoleaf/utils/updateEffectName.ts
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+
+import { constants } from 'nanoleaf/definitions';
+
+const { endpoints } = constants;
+
+interface UpdateEffectNameArgs {
+  ipAddress: string;
+  authToken: string;
+  existingName: string;
+  newName: string;
+}
+
+/** Example body request
+ * {
+ *  "command": "rename",
+ *  "animName": "My Flow Animation",
+ *  "newName": "My New Flow Animation"
+ * }
+ */
+
+const updateEffectName = async ({
+  existingName,
+  authToken,
+  ipAddress,
+  newName,
+}: UpdateEffectNameArgs): Promise<void> => {
+  try {
+    await fetch(endpoints.update.effect(ipAddress, authToken), {
+      method: 'PUT',
+      body: JSON.stringify({
+        write: {
+          command: 'rename',
+          animName: existingName,
+          newName,
+        },
+      }),
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default updateEffectName;
