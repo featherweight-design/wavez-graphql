bb913b2730ef920b0b085bcb29316a06ffbacf60
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-25 18:19:33 -0600

    chore: Add noDevicesByType error for Devices

--- src/device/definitions/errors.ts
+++ src/device/definitions/errors.ts
@@ -1,4 +1,12 @@
+import { DeviceType } from '@prisma/client';
+
 import { ErrorResponse } from 'types';
+
+const friendlyDeviceNameMap = {
+  [DeviceType.HUE]: 'Philips Hue',
+  [DeviceType.LIFX]: 'LIFX',
+  [DeviceType.NANOLEAF]: 'Nanoleaf',
+};
 
 const errors = {
   deviceNotFound: (deviceId: string): ErrorResponse => ({
@@ -11,6 +19,11 @@ const errors = {
     message: `Device by id ${deviceId} has no associated authToken`,
     friendlyMessage:
       "It looks like this device hasn't been authenticated. Please try again.",
+  }),
+  noDevicesByType: (type: DeviceType): ErrorResponse => ({
+    status: 404,
+    message: `No devices by type ${type} found`,
+    friendlyMessage: `It looks like there aren't any saved ${friendlyDeviceNameMap[type]} device hasn't been authenticated. Please try again.`,
   }),
 };
 
