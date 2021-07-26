b390f94e8622d665aa081824723289a8b2ed27e9
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-25 18:21:19 -0600

    refactor: Migrate authTokenNotFound error to errors

--- src/nanoleaf/definitions/errors.ts
+++ src/nanoleaf/definitions/errors.ts
@@ -7,6 +7,12 @@ const errors = {
     friendlyMessage:
       'Unable to authenticate with Nanoleaf device. Ensure that your device is ready to authenticate by holding down the power button until the lights start blinking (5-7 seconds).',
   }),
+  authTokenNotFound: (id: string): ErrorResponse => ({
+    status: 404,
+    message: `Nanoleaf authTokeb by id ${id} not found`,
+    friendlyMessage:
+      "We can't find a Nanoleaf device with those credentials. Please try again.",
+  }),
   deviceConflict: (ipAddress: string): ErrorResponse => ({
     status: 409,
     message: `Device conflict by IP Address ${ipAddress}`,
