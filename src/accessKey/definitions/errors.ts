d1c92ac5ad5a39141bb08123a9bf43ccf809fdf2
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-08-21 16:52:36 -0600

    chore: Add new AccessKey errors

--- src/accessKey/definitions/errors.ts
+++ src/accessKey/definitions/errors.ts
@@ -1,10 +1,21 @@
 import { ErrorResponse } from 'types';
 
 const errors: Record<string, ErrorResponse> = {
+  expired: {
+    status: 400,
+    message: 'Access key expired',
+    friendlyMessage:
+      'This access key is expired. Please reach out to the person who invited you for assistance.',
+  },
   notFound: {
     status: 404,
     message: 'Access key does not exists',
     friendlyMessage: 'This access key does not exist or has already been used.',
+  },
+  wrongEmail: {
+    status: 400,
+    message: 'Wrong email provided',
+    friendlyMessage: 'Incorrect email provided with access key',
   },
 };
 
