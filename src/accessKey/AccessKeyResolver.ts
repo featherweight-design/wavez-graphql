41bba7f3ad658d81f830bcb14178ba39072ffc3d
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-08-22 11:57:16 -0600

    build: deleteAccessKeyById mutation

--- src/accessKey/AccessKeyResolver.ts
+++ src/accessKey/AccessKeyResolver.ts
@@ -1,9 +1,11 @@
+import { UserInputError } from 'apollo-server';
 import { Arg, Ctx, Directive, Mutation, Resolver } from 'type-graphql';
 import dayjs from 'dayjs';
 
 import { Context, RoleEnum } from 'types';
-import AccessKey from './AccessKey';
 import { User } from 'user';
+import AccessKey from './AccessKey';
+import { errors } from './definitions';
 
 @Resolver(AccessKey)
 class AccessKeyResolver {
@@ -26,6 +28,30 @@ class AccessKeyResolver {
       });
 
       return accessKey;
+    } catch (error) {
+      console.error(error);
+
+      throw error;
+    }
+  }
+
+  @Directive('@authenticated')
+  @Directive(`@authorized(role: ${RoleEnum.ADMIN})`)
+  @Mutation(() => AccessKey)
+  async deleteAccessKeyById(
+    @Arg('id') id: string,
+    @Ctx() { prisma }: Context
+  ): Promise<string> {
+    try {
+      const accessKey = await prisma.accessKey.delete({
+        where: { id },
+      });
+
+      if (!accessKey) {
+        throw new UserInputError(JSON.stringify(errors.notFound));
+      }
+
+      return id;
     } catch (error) {
       console.error(error);
 
