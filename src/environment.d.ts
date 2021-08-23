6951a5cf075bb8152378e46996f5126a55c15f44
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-08-22 18:46:48 -0600

    chore: Add SENDGRID_API_KEY to environment TS types

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       SECRET: string;
+      SENDGRID_API_KEY: string;
       NODE_ENV: 'development' | 'production';
       PORT?: string;
     }
