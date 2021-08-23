ed9d2b59d37a456ee9e12ce731bf35a7190c3d77
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-08-22 18:46:15 -0600

    build: validateNewAccessKey utility

---
+++ src/accessKey/utilities/index.ts
@@ -0,0 +1,3 @@
+import validateNewAccessKey from './validateNewAccessKey';
+
+export { validateNewAccessKey };
---
+++ src/accessKey/utilities/validateNewAccessKey.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+import { UserInputError } from 'apollo-server';
+
+import { errors as userErrors } from 'user/definitions';
+import { errors } from '../definitions';
+
+const validateNewAccessKey = async (
+  prisma: PrismaClient,
+  email: string
+): Promise<void> => {
+  //* Check if email is already used
+  const doesUserExist = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (doesUserExist) {
+    throw new UserInputError(JSON.stringify(userErrors.userAlreadyExists));
+  }
+
+  //* Check is user has already been sent an access key
+  const doesAccessKeyExistByEmail = await prisma.accessKey.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (doesAccessKeyExistByEmail) {
+    throw new UserInputError(JSON.stringify(errors.alreadyExists));
+  }
+};
+
+export default validateNewAccessKey;
