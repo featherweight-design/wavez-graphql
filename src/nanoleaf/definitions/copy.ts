efbd11ef8f7c9e92c9816a2736224f43db6c6f05
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-08-28 14:11:32 -0600

    chore: Add descriptions to NanoleafAuthToken resolvers

---
+++ src/nanoleaf/definitions/copy.ts
@@ -0,0 +1,9 @@
+const descriptions = {
+  authenticateWithDeviceByUserId:
+    'Authenticates a user with a given Nanoleaf device by IP Address that is on their local network by creating a new NanoleafAuthToken, new Device, and, optionally, new Palettes associated with the Nanoleaf device',
+  deleteNanoleafAuthToken: 'Deletes a given NanoleafAuthToken by ID',
+};
+
+const copy = { descriptions };
+
+export default copy;
